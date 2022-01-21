89b3f40c991bfc9ea49e8d94c8b6b60ca5acf0b9
Markus B <m.bartels@socialpoints.online>
2022-01-21 16:49:07 +0100

    Initial commit

---
+++ src/Core/Request.php
@@ -0,0 +1,301 @@
+<?php
+
+namespace Vfw\Core;
+
+class Request
+{
+    /**
+     * @var string
+     */
+    private const API_URL = 'https://tb.mbdev.zone:9666/';
+
+    /**
+     * @var string
+     */
+    private const API_SECRET = 'BAIC4WFAfJI4ABXrFfOhY9qAtH93fajQSAVlWSflSsPfba8csgjr2qBo33Olg2NeIQ7c39XoDgiMgcvD_ciTJyWzhEa3gVPzWVbvxKEUlayn14ooF3aZV7_pT7kXx7oR';
+
+    /**
+     * @var array
+     */
+    private const API_EP_CREATE_USER         = ['ep' => 'vfw/user/new', 'type' => 'POST'];
+    private const API_EP_DETELE_USER         = ['ep' => 'vfw/user/delete', 'type' => 'DELETE'];
+    private const API_EP_CHANGE_USER_NAME    = ['ep' => 'vfw/user/name', 'type' => 'PATCH'];
+    private const API_EP_CHANGE_USER_ROLE    = ['ep' => 'vfw/user/rights', 'type' => 'PATCH'];
+    private const API_EP_DELETE_USER_CONTENT = ['ep' => 'vfw/space/remove', 'type' => 'POST'];
+    private const API_EP_ADD_MEMBERSHIP      = ['ep' => 'vfw/user/add', 'type' => 'POST'];
+    private const API_EP_REMOVE_MEMBERSHIP   = ['ep' => 'vfw/user/remove', 'type' => 'DELETE'];
+    private const API_EP_CREATE_SPACE        = ['ep' => 'vfw/space/create', 'type' => 'POST'];
+    private const API_EP_RESET_SPACE         = ['ep' => 'vfw/space/reset', 'type' => 'PURGE'];
+    private const API_EP_DELETE_SPACE        = ['ep' => 'vfw/space/delete', 'type' => 'DELETE'];
+    private const API_EP_CREATE_SESSION      = ['ep' => 'vfw/user/login', 'type' => 'POST'];
+    private const API_EP_REMOVE_SESSION      = ['ep' => 'vfw/user/logout', 'type' => 'POST'];
+
+    /**
+     * createUser
+     * 
+     * @param string $email
+     * @param string $username
+     * @return string|bool
+     */
+    public function createUser($email, $username)
+    {
+        $postData = [
+            'email' => $email,
+            'nickname' => $username
+        ];
+
+        return $this->send(self::API_EP_CREATE_USER, $postData);
+    }
+
+    /**
+     * deleteUser
+     * 
+     * @param string $userId
+     * @return string|bool
+     */
+    public function deleteUser($userId)
+    {
+        $postData = [
+            'id' => $userId
+        ];
+
+        return $this->send(self::API_EP_DETELE_USER, $postData);
+    }
+
+    /**
+     * changeUserName
+     * 
+     * @param string $userId
+     * @param string $name
+     * @return string|bool
+     */
+    public function changeUserName($userId, $name)
+    {
+        $postData = [
+            'user' => $userId,
+            'name' => $name
+        ];
+
+        return $this->send(self::API_EP_CHANGE_USER_NAME, $postData);
+    }
+
+    /**
+     * changeUserRole
+     * 
+     * @param string $userId
+     * @param string $spaceId
+     * @param string $role
+     * @return string|bool
+     */
+    public function changeUserRole($userId, $spaceId, $role)
+    {
+        $postData = [
+            'user' => $userId,
+            'space' => $spaceId,
+            'role' => $role
+        ];
+
+        return $this->send(self::API_EP_CHANGE_USER_ROLE, $postData);
+    }
+
+    /**
+     * deleteUserContent
+     * 
+     * @param string $userId
+     * @param string $spaceId
+     * @return string|bool
+     */
+    public function deleteUserContent($userId, $spaceId)
+    {
+        $postData = [
+            'user' => $userId,
+            'space' => $spaceId
+        ];
+
+        return $this->send(self::API_EP_DELETE_USER_CONTENT, $postData);
+    }
+
+    /**
+     * createMembership
+     * 
+     * @param string $userId
+     * @param string $spaceId
+     * @param string $role
+     * @return string|bool
+     */
+    public function createMembership($userId, $spaceId, $role)
+    {
+        $postData = [
+            'user' => $userId,
+            'space' => $spaceId,
+            'role' => $role
+        ];
+
+        return $this->send(self::API_EP_ADD_MEMBERSHIP, $postData);
+    }
+
+    /**
+     * deleteMembership
+     * 
+     * @param string $userId
+     * @param string $spaceId
+     * @return string|bool
+     */
+    public function deleteMembership($userId, $spaceId)
+    {
+        $postData = [
+            'user' => $userId,
+            'space' => $spaceId
+        ];
+
+        return $this->send(self::API_EP_REMOVE_MEMBERSHIP, $postData);
+    }
+
+    /**
+     * createSpace
+     * 
+     * @param string $name
+     * @return string|bool
+     */
+    public function createSpace($name)
+    {
+        $postData = [
+            'name' => $name
+        ];
+
+        return $this->send(self::API_EP_CREATE_SPACE, $postData);
+    }
+
+    /**
+     * resetSpace
+     * 
+     * @param string $spaceId
+     * @return string|bool
+     */
+    public function resetSpace($spaceId)
+    {
+        $postData = [
+            'space' => $spaceId
+        ];
+
+        return $this->send(self::API_EP_RESET_SPACE, $postData);
+    }
+
+    /**
+     * deleteSpace
+     * 
+     * @param string $spaceId
+     * @return string|bool
+     */
+    public function deleteSpace($spaceId)
+    {
+        $postData = [
+            'id' => $spaceId
+        ];
+
+        return $this->send(self::API_EP_DELETE_SPACE, $postData);
+    }
+
+    /**
+     * createUserSession
+     * 
+     * @param string $userId
+     * @return string|bool
+     */
+    public function createUserSession($userId)
+    {
+        $postData = [
+            'id' => $userId
+        ];
+
+        return $this->send(self::API_EP_CREATE_SESSION, $postData);
+    }
+
+    /**
+     * removeUserSession
+     * 
+     * @param string $userId
+     * @return string|bool
+     */
+    public function removeUserSession($userId)
+    {
+        $postData = [
+            'user' => $userId
+            //'token' => $token
+        ];
+
+        return $this->send(self::API_EP_REMOVE_SESSION, $postData);
+    }
+
+    /**
+     * checkResult
+     * 
+     * @param array $endpoint
+     * @param array $result
+     * @return bool
+     */
+    private function checkResult($endpoint, $result)
+    {
+        if (!is_array($result) || !isset($result['status']) || !is_bool($result['status']))
+            return false;
+        
+        if ($result['status']) {
+            switch ($endpoint) {
+                case self::API_EP_CREATE_USER:
+                case self::API_EP_CREATE_SPACE: {
+                    if (!isset($result['id']))
+                        return false;
+                } break;
+
+                case self::API_EP_CREATE_SESSION: {
+                    if (!isset($result['token']))
+                        return false;
+                } break;
+            }
+        }
+        else {
+            if (!isset($result['error']))
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * send
+     * 
+     * @param array $endpoint
+     * @param mixed $postData
+     * @return array|bool
+     */
+    private function send($endpoint, $postData)
+    {
+        $url = self::API_URL. $endpoint['ep'];
+        $payload = json_encode($postData);
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $endpoint['type']);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type:application/json',
+                                              'Accept:application/json',
+                                              'X-Spacedeck-API-Token:'. self::API_SECRET]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($ch);
+        if ($result !== false) {
+            $jData = json_decode($result, true);
+            if ($this->checkResult($endpoint, $jData))
+                $result = $jData;
+            else {
+                // TODO: ...
+                $result = false;
+            }
+        }
+
+        curl_close($ch);
+
+        return $result;
+    }
+}
+
+?>
---
+++ src/SpacedeckApi.php
@@ -0,0 +1,243 @@
+<?php
+
+namespace Vfw;
+
+use Vfw\Core\Request;
+
+class SpacedeckApi
+{
+    /**
+     * @var string
+     */
+    public const USER_ROLE_ADMIN  = "admin";
+    public const USER_ROLE_EDITOR = "editor";
+    public const USER_ROLE_VIEWER = "viewer";
+
+    /**
+     * createUser
+     * 
+     * @param string $email
+     * @param string $username
+     * @return string|bool
+     */
+    public function createUser($email, $username)
+    {
+        $req = new Request();
+        $res = $req->createUser($email, $username);
+        if ($res !== false) {
+            if ($res['status']) {
+                return $res['id'];
+            }
+            
+            // TODO: else ... ?
+        }
+
+        return false;
+    }
+
+    /**
+     * deleteUser
+     * 
+     * @param string $userId
+     * @return bool
+     */
+    public function deleteUser($userId)
+    {
+        $req = new Request();
+        $res = $req->deleteUser($userId);
+        if ($res !== false) {
+            return $res['status'];
+        }
+
+        return false;
+    }
+
+    /**
+     * changeUserName
+     * 
+     * @param string $userId
+     * @param string $name
+     * @return bool
+     */
+    public function changeUserName($userId, $name)
+    {
+        $req = new Request();
+        $res = $req->changeUserName($userId, $name);
+        if ($res !== false) {
+            return $res['status'];
+        }
+
+        return false;
+    }
+
+    /**
+     * changeUserRole
+     * 
+     * @param string $userId
+     * @param string $spaceId
+     * @param string $role
+     * @return bool
+     */
+    public function changeUserRole($userId, $spaceId, $role)
+    {
+        $req = new Request();
+        $res = $req->changeUserRole($userId, $spaceId, $role);
+        if ($res !== false) {
+            return $res['status'];
+        }
+
+        return false;
+    }
+
+    /**
+     * deleteUserContent
+     * 
+     * @param string $userId
+     * @param string $spaceId
+     * @return bool
+     */
+    public function deleteUserContent($userId, $spaceId)
+    {
+        $req = new Request();
+        $res = $req->deleteUserContent($userId, $spaceId);
+        if ($res !== false) {
+            return $res['status'];
+        }
+
+        return false;
+    }
+
+    /**
+     * createMembership
+     * 
+     * @param string $userId
+     * @param string $spaceId
+     * @param string $role
+     * @return bool
+     */
+    public function createMembership($userId, $spaceId, $role)
+    {
+        $req = new Request();
+        $res = $req->createMembership($userId, $spaceId, $role);
+        if ($res !== false) {
+            return $res['status'];
+        }
+
+        return false;
+    }
+
+    /**
+     * deleteMembership
+     * 
+     * @param string $userId
+     * @param string $spaceId
+     * @return bool
+     */
+    public function deleteMembership($userId, $spaceId)
+    {
+        $req = new Request();
+        $res = $req->deleteMembership($userId, $spaceId);
+        if ($res !== false) {
+            return $res['status'];
+        }
+
+        return false;
+    }
+
+    /**
+     * createSpace
+     * 
+     * @param string $name
+     * @return string|bool
+     */
+    public function createSpace($name)
+    {
+        $req = new Request();
+        $res = $req->createSpace($name);
+        if ($res !== false) {
+            if ($res['status']) {
+                return $res['id'];
+            }
+            
+            // TODO: else ... ?
+        }
+
+        return false;
+    }
+
+    /**
+     * resetSpace
+     * 
+     * @param string $spaceId
+     * @return bool
+     */
+    public function resetSpace($spaceId)
+    {
+        $req = new Request();
+        $res = $req->resetSpace($spaceId);
+        if ($res !== false) {
+            return $res['status'];
+        }
+
+        return false;
+    }
+
+    /**
+     * deleteSpace
+     * 
+     * @param string $spaceId
+     * @return bool
+     */
+    public function deleteSpace($spaceId)
+    {
+        $req = new Request();
+        $res = $req->deleteSpace($spaceId);
+        if ($res !== false) {
+            return $res['status'];
+        }
+
+        return false;
+    }
+
+    /**
+     * createUserSession
+     * 
+     * @param string $userId
+     * @return string|bool
+     */
+    public function createUserSession($userId)
+    {
+        $req = new Request();
+        $res = $req->createUserSession($userId);
+        if ($res !== false) {
+            if ($res['status']) {
+                setcookie("sdsession", $res['token']);
+                return $res['token'];
+            }
+            
+            // TODO: else ... ?
+        }
+
+        return false;
+    }
+
+    /**
+     * removeUserSession
+     * 
+     * @param string $userId
+     * @return string|bool
+     */
+    public function removeUserSession($userId)
+    {
+        $req = new Request();
+        $res = $req->removeUserSession($userId);
+        if ($res !== false) {
+            setcookie("sdsession", "", time() - 3600);
+            return $res['status'];
+        }
+
+        return false;
+    }
+}
+
+?>
